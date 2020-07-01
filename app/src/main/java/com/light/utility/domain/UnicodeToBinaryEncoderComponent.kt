4fcce0c59888860fec58d92ec8f2302cc57dfced
krbgh205760 <krbgh205760@naver.com>
2020-07-01 21:57:23 +0900

    Feature/binaryencoder (#6)
    Add UnicodeToBinaryEncoder
    
    * Change calling order in MainFrame for half-measure error in Base64Decoder

---
+++ app/src/main/java/com/light/utility/domain/UnicodeToBinaryEncoderComponent.kt
@@ -0,0 +1,26 @@
+package com.light.utility.domain
+
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class UnicodeToBinaryEncoderComponent : TextUtilComponent {
+
+	@ExperimentalCoroutinesApi
+	private val state: MutableStateFlow<String> by lazy {
+		MutableStateFlow("")
+	}
+
+	override fun getState(): StateFlow<String> = state
+
+	override fun apply(text: String) {
+		state.value = convertStringToBinary(text)
+	}
+
+	private fun convertStringToBinary(text: String): String {
+		var binStr = ""
+		for (char in text.toByteArray())
+			binStr += char.toUInt().toString(radix = 2)
+		return binStr
+	}
+}
--- app/src/main/java/com/light/utility/ui/MainFrame.kt
+++ app/src/main/java/com/light/utility/ui/MainFrame.kt
@@ -3,6 +3,7 @@ package com.light.utility.ui
 import com.light.utility.domain.Base64DecoderComponent
 import com.light.utility.domain.Base64EncoderComponent
 import com.light.utility.domain.TextUtilComponent
+import com.light.utility.domain.UnicodeToBinaryEncoderComponent
 import com.light.utility.domain.UtilComponent
 import java.awt.BorderLayout
 import java.awt.Dimension
@@ -14,59 +15,56 @@ import javax.swing.JTextArea
 
 class MainFrame : JFrame() {
 
-    private val textArea by lazy {
-        JTextArea()
-    }
+	private val textArea by lazy {
+		JTextArea()
+	}
 
-    val utils by lazy {
-        mutableListOf<UtilComponent>()
-    }
+	val utils by lazy {
+		mutableListOf<UtilComponent>()
+	}
 
-    init {
-        setupView()
-        setupEventListeners()
+	init {
+		setupView()
+		setupEventListeners()
 
-        val base64EncoderComponent = Base64EncoderComponent()
-            .also {
-                utils.add(it)
-            }
-        Base64EncoderFrame(base64EncoderComponent)
+		val binaryEncoderComponent = UnicodeToBinaryEncoderComponent().also { utils.add(it) }
+		UnicodeToBinaryEncoderFrame(binaryEncoderComponent)
 
-		val base64DecoderComponent = Base64DecoderComponent()
-			.also {
-				utils.add(it)
-			}
+		val base64EncoderComponent = Base64EncoderComponent().also { utils.add(it) }
+		Base64EncoderFrame(base64EncoderComponent)
+
+		val base64DecoderComponent = Base64DecoderComponent().also { utils.add(it) }
 		Base64DecoderFrame(base64DecoderComponent)
 	}
 
-    private fun setupView() {
-        defaultCloseOperation = EXIT_ON_CLOSE
-        val screenSize = Toolkit.getDefaultToolkit().screenSize
-        size = Dimension(screenSize.width / 2, screenSize.height / 2)
-        isVisible = true
+	private fun setupView() {
+		defaultCloseOperation = EXIT_ON_CLOSE
+		val screenSize = Toolkit.getDefaultToolkit().screenSize
+		size = Dimension(screenSize.width / 2, screenSize.height / 2)
+		isVisible = true
 
-        layout = BorderLayout()
+		layout = BorderLayout()
 
-        add(textArea, BorderLayout.CENTER)
-    }
+		add(textArea, BorderLayout.CENTER)
+	}
 
-    private fun setupEventListeners() {
-        textArea.apply {
-            addKeyListener(object : KeyListener {
-                override fun keyTyped(p0: KeyEvent?) {
-                }
+	private fun setupEventListeners() {
+		textArea.apply {
+			addKeyListener(object : KeyListener {
+				override fun keyTyped(p0: KeyEvent?) {
+				}
 
-                override fun keyPressed(p0: KeyEvent?) {
-                }
+				override fun keyPressed(p0: KeyEvent?) {
+				}
 
-                override fun keyReleased(p0: KeyEvent?) {
-                    utils
-                        .filterIsInstance(TextUtilComponent::class.java)
-                        .forEach {
-                            it.apply(text)
-                        }
-                }
-            })
-        }
-    }
+				override fun keyReleased(p0: KeyEvent?) {
+					utils
+						.filterIsInstance(TextUtilComponent::class.java)
+						.forEach {
+							it.apply(text)
+						}
+				}
+			})
+		}
+	}
 }

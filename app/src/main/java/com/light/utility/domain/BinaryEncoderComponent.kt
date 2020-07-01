47a22a92a130b520915cff5ac5454e82b79b9bda
k <krbgh205760@naver.com>
2020-07-01 14:55:12 +0900

    Use toUInt() to convert

--- app/src/main/java/com/light/utility/domain/BinaryEncoderComponent.kt
+++ app/src/main/java/com/light/utility/domain/BinaryEncoderComponent.kt
@@ -7,14 +7,14 @@ import java.util.*
 
 class BinaryEncoderComponent : TextUtilComponent {
 
-    @ExperimentalCoroutinesApi
-    private val state: MutableStateFlow<String> by lazy {
-        MutableStateFlow("")
-    }
+	@ExperimentalCoroutinesApi
+	private val state: MutableStateFlow<String> by lazy {
+		MutableStateFlow("")
+	}
 
-    override fun getState(): StateFlow<String> = state
+	override fun getState(): StateFlow<String> = state
 
-    override fun apply(text: String) {
-        state.value = String(text.toByteArray())
-    }
+	override fun apply(text: String) {
+		state.value = text.toUInt().toString(radix = 2)
+	}
 }

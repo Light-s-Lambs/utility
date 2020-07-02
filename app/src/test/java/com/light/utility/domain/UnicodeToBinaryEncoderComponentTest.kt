840737980e63594db11e4fc11870a868a3163fac
jongsu253 <jongsu253@gmail.com>
2020-07-03 03:19:57 +0900

    Add test case

---
+++ app/src/test/java/com/light/utility/domain/UnicodeToBinaryEncoderComponentTest.kt
@@ -0,0 +1,22 @@
+package com.light.utility.domain
+
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+class UnicodeToBinaryEncoderComponentTest {
+
+    lateinit var component: UnicodeToBinaryEncoderComponent
+
+    @Before
+    fun setUp() {
+        component = UnicodeToBinaryEncoderComponent()
+    }
+
+    @Test
+    fun testGetState_whenSubscribeEvent_shouldReceiveEmptyString() = runBlockingTest {
+        Assert.assertEquals(component.getState().firstOrNull(), "")
+    }
+}

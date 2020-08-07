b0feb8c6fb9e93c5d3a18ccef3fa78b020359906
DploY <starj1024@gmail.com>
2020-08-07 18:20:43 +0900

    Implement oct decoding logics

--- app/src/main/java/com/light/utility/domain/OctDecoderComponent.kt
+++ app/src/main/java/com/light/utility/domain/OctDecoderComponent.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.StateFlow
 
 class OctDecoderComponent : TextUtilComponent {
     companion object {
+        const val OCT_DECODING_INPUT_ERROR = "Check your Input"
         const val EMPTY_STRING = ""
     }
 
@@ -19,6 +20,40 @@ class OctDecoderComponent : TextUtilComponent {
 
     @ExperimentalCoroutinesApi
     override fun apply(text: String) {
-        state.value = text
+        val decoded_oct_string = convertOctToString(text)
+
+        if (decoded_oct_string.isEmpty())
+            state.value = OCT_DECODING_INPUT_ERROR
+        else
+            state.value = decoded_oct_string
+    }
+
+    private fun convertOctToString(octStr: String): String =
+        if (isOctString(octStr)) {
+            octStr.split("\\s".toRegex())
+                .map { octDigitToChar(it) }
+                .joinToString("")
+        } else {
+            EMPTY_STRING
+        }
+
+    private fun isOctString(octStr: String): Boolean {
+        val octArray = octStr.toByteArray()
+
+        for (char in octArray) {
+            if (!isOctDigit(char.toChar()) && !char.toChar().isWhitespace())
+                return false
+        }
+
+        return true
+    }
+
+    private fun isOctDigit(char: Char): Boolean {
+        val octChars = "01234567"
+        return char.isDigit() || (char in octChars)
+    }
+
+    private fun octDigitToChar(octDigit: String): Char {
+        return octDigit.toLong(radix = 8).toChar()
     }
 }

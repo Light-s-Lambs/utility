625a0cdeba8f1dd9248a13035f8e65b611fa4f89
krbgh205760 <krbgh205760@naver.com>
2020-10-05 10:21:21 +0900

    Base64DecoderPresenter의 프레임 추가

--- app/src/main/java/com/light/utility/ui/Base64DecoderPresenter.kt
+++ app/src/main/java/com/light/utility/ui/Base64DecoderPresenter.kt
@@ -1,7 +1,10 @@
 package com.light.utility.ui
 
+import com.light.utility.domain.TextUtilComponent
+
 class Base64DecoderPresenter constructor(
-    override val view: Base64DecoderContract.View
+    override val view: Base64DecoderContract.View,
+    component: TextUtilComponent
 ) : Base64DecoderContract.Presenter {
     override fun onUserEdited(text: String) {
 
